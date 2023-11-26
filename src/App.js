8cc9abe23e2cd6c4534b91dbda201c7615d07ef2
Hanzla Saadi <hanzalasaadi@gmail.com>
2023-11-27 00:45:36 +0500

    bug fix home page router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 // import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import All from "./components/Home";
 import "./MyStyle.css";
 import Navbar from "./components/navbar";
+import HomePage from "./components/Home";
 
 function App() {
   <BrowserRouter>
-  <Routes>  
-
-  <Route path="/All" element={<All />} />
-  <Route path="/Navbar" element={<Navbar />} />
-
-
-
-  </Routes>
-    </BrowserRouter>
-    }
+    <Routes>
+      <Route path="/All" element={<HomePage />} />
+      <Route path="/Navbar" element={<Navbar />} />
+    </Routes>
+  </BrowserRouter>;
+}
 
 export default App;
